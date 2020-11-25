c703d404d0432fd0060be0dd34a0e478a09b092f
bilal-62210 <bilal.benelkadi@viacesi.fr>
2020-11-25 22:54:31 +0100

    main

---
+++ Prosit5.2/CLimages.h
@@ -0,0 +1,14 @@
+//CLimages.h
+#pragma once
+
+using namespace System;
+using namespace System::Drawing;
+
+namespace NS_composants
+{
+	public ref class CLimages
+	{
+	public:
+		System::Drawing::Bitmap^ acquisitionImage(String^);
+	};
+}
---
+++ prosit5/Form1.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+// #include "Form1.h"
+
+//MyForm.cpp
+#include "MyForm.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThreadAttribute]
+int __clrcall WinMain(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	testWin::MyForm monFormulaire;
+}
+
